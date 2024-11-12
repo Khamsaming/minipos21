c80da7defee75b0994e9aae193b5150d0431f379
khamsaming <lel.pmc166@gmail.com>
2024-11-12 09:03:13 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Hello App Vue By MING
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Minipos</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1> HELLO Laravel 11 </h1><hr>
+    <div id="app-vue"></div>
+    @vite('resources/js/app.js')
+</body>
+</html>
